4a983e32fc7e98cfe0121aae11b4af80dc196e5c
kmott170 <kdittman@fullsail.edu>
2015-01-24 08:06:56 -0500

    Concatination, Booleans & Text Strings
    learning concatenation, booleans and text strings

---
+++ Testing2/js/script.js
@@ -0,0 +1,102 @@
+/**
+ * Created by TempestStorm on 1/24/15.
+ */
+
+//alert("This is an alert! Warning! Warning!");
+
+//Strings (This is any text variable)
+//These are used to when trying to tell a difference between a variable & random text
+
+//Example 1
+
+var appleName ="Granny smith";
+
+console.log(appleName);
+
+//Example 2
+
+var showName = "Bones";
+
+console.log(showName);
+
+//Example 3
+
+var berryName ="Blueberry";
+
+console.log(berryName);
+
+//Escaping Character - \
+
+//Example 1
+
+var sentence ="I don\'t care!";
+console.log(sentence);
+
+//Example 2
+
+var phrase ="I don\'t have time for this!";
+console.log(phrase);
+
+//Multiple Lines - starting a new line
+
+//Example 1
+
+var sentence1 ="I don\'t have a clue \nI know you don\'t.";
+console.log(sentence1);
+
+//Example 2
+
+var phrase1 ="I can\'t hand this right now! \nI don\'t particularly care.";
+console.log(phrase1);
+
+//Example 3
+
+var sentence2 ="I do not suck. \nYes you do.";
+console.log(sentence2);
+
+//Booleans (is like a lightswitch - it's either on or off)
+//true or false - Does not equal "true" or "false"
+
+var yes = true;
+var no = false;
+
+//THIS IS NOT A BOOLEAN
+var wrong = "true";
+
+//Concatenation - combining text strings
+//You use the + sign for text & numbers
+
+//Example 1
+
+var firstName = "Kermit";
+var lastName = "The Frog";
+
+var fullName = firstName +" " +lastName;
+console.log(firstName);
+console.log(fullName+ " is my favorite muppet");
+
+//Test 1
+
+var firstName1 = "Snow";
+var lasName1 ="White";
+
+var fullName2 = firstName1 +" "+lasName1;
+console.log(firstName1);
+
+//Test 2
+
+var firstName2 ="Black";
+var lastName2 ="Cauldron";
+
+var fullName3= firstName2 +" "+lastName2;
+console.log(fullName3+ " is my favorite movie.");
+
+//Example 2
+
+var firstName3 ="Neverending";
+var lastName3 ="Story";
+
+var fullName4= firstName3 +" "+lastName3;
+console.log(firstName3);
+console.log(fullName4+ " is my second favorite movie");
+
